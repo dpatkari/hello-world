90f3548b6065c9ab25a235a9eeb945474b4c8819
Durgesh Patkari <durgeshpatkari@gmail.com>
2017-12-09 01:34:22 +0530

    added sample program :tada:

---
+++ hello.c
@@ -0,0 +1,19 @@
+void main(){
+	int a =1;
+	int b=2;
+	printf("A + B = %d",add(a,b));
+	printf("A - B = %d",sub(a,b));
+	printf("A * B = %d",mul(a,b));
+}
+
+int add(int a, int b){
+	
+}
+
+int sub(int a, int b){
+	
+}
+
+int mul(int a, int b){
+	
+}
